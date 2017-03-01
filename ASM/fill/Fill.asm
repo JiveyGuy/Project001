ee77721afef33f8708d0479e577d00a64ea0453f
JiveyGuy <jason.ivey.not.micheal@gmail.com>
2017-03-01 12:53:03 -0700

    Added fill.asm

---
+++ ASM/fill/Fill.asm
@@ -0,0 +1,25 @@
+(Loop)
+@16384
+D = A
+@SCREEN
+M = D
+@24576
+D = M - 1
+@FILL
+D;JEZ
+@LOOP
+0;JMP
+
+(FILL)
+@24576
+D = M - 1
+@LOOP
+D;JLT
+@32
+D = A
+@SCREEN
+M = M + D
+A = M
+M = -1
+@FILL
+0;JMP
