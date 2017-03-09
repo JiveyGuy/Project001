e7f6230c876f2266d84b0ba0284e20653bea655b
Jason Ivey <jason.ivey.not.micheal@gmail.com>
2017-03-08 20:06:19 -0700

    Add files via upload

--- ASM/fill/fill.asm
+++ ASM/fill/fill.asm
@@ -1,25 +1,42 @@
-(LOOP)
-@16384
-D = A
 @SCREEN
+D = A
+@POINTER
 M = D
-@KBD
-D = M
-@FILL
-D;JGT
-@LOOP
-0;JMP
 
-(FILL)
-@KBD
-D = M
-@LOOP
-D;JEQ
-@1
-D = A
-@SCREEN
-M = M + D
-A = M
-M = -1
-@FILL
+(CHECK)
+	@KBD
+	D = M
+	@val
+	M = 0
+	@KBD
+	D = M
+	@FILL
+	D;JEQ	
+
+	(NEG)
+	@val
+	M = -1
+	
+	(FILL)
+	@val
+	D = M
+	@POINTER
+	A = M
+	M = D
+
+	@POINTER
+	M = M + 1
+	D = M
+
+	@KBD
+	D = D - A
+	@CHECK
+D;JLT
+	@SCREEN
+	D = A
+	@POINTER
+	M = D
+@CHECK
 0;JMP
+	
+	
