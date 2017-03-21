c3c5d3856338bd7379d688ebdd334271628643f0
Jason Ivey <jason.ivey.not.micheal@gmail.com>
2017-03-21 16:53:15 -0600

    Add files via upload

---
+++ Assembler/Encode.java
@@ -0,0 +1,19 @@
+public class Encode{
+  public String[] data;
+  public Encode(Input in){
+    String[] input = in.file;
+  }
+  
+  private void multiplex(String[] input){
+    String[] output = new String[input.length];
+    for( int x = 0; x < input.length; X++){
+      switch ( input[x].charAt(0) ){
+        case '@' : output[x] =  
+      }
+    }
+  }
+  
+  private String aCase(String input){
+    
+  }
+}
